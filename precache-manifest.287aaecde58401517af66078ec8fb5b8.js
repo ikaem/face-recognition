4a7df846a0ea4125dff0ff5b82e445461efdc8fd
Karlo Marinovic <36795522+ikaem@users.noreply.github.com>
2019-12-11 14:38:24 +0100

    Updates

---
+++ precache-manifest.287aaecde58401517af66078ec8fb5b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afd39bf0131c833d8519f16a2d5d5ef",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "ac4f5f56739f9fe98c01",
+    "url": "/face-recognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5ad3e66e5fefa0f558a8",
+    "url": "/face-recognition/static/css/main.8d862ae5.chunk.css"
+  },
+  {
+    "revision": "ac4f5f56739f9fe98c01",
+    "url": "/face-recognition/static/js/2.fedc8dac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/face-recognition/static/js/2.fedc8dac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ad3e66e5fefa0f558a8",
+    "url": "/face-recognition/static/js/main.4fb57d66.chunk.js"
+  },
+  {
+    "revision": "ee999e6fe6429e9c457c",
+    "url": "/face-recognition/static/js/runtime-main.775a38bc.js"
+  }
+]);
